7327995936cfe6f68bab9edffcf8ae1939ba7dcb
Eric.Guo <eric.guo@oceania.com>
2018-06-21 17:19:40 +0800

    取消暂时不用的路由

--- routes/web.php
+++ routes/web.php
@@ -15,29 +15,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 //定义路由组
-Route::get('/test','WebCrawlerController@test');
+//Route::get('/test','WebCrawlerController@test');
 
 
 //TODO TEST
-Route::get('/baidu','WebCrawlerController@baidu');
-
-Route::get('/wxarticle','WebCrawlerController@article');
-
-Route::get('/github','WebCrawlerController@github');
-
-Route::get('/gitee','WebCrawlerController@gitee');
-
-Route::get('/snoopy','WebCrawlerController@snooy');
-
-Route::get('/amazon','WebCrawlerController@amazon');
-
-Route::get('/cookie','WebCrawlerController@cookie');
-
-Route::get('/checkcurl','WebCrawlerController@checkcurl');
-
-Route::get('/zhihu','WebCrawlerController@zhihu');
-
-Route::get('/test_spider','WebCrawlerController@test_spider');
+//Route::get('/baidu','WebCrawlerController@baidu');
+//
+//Route::get('/wxarticle','WebCrawlerController@article');
+//
+//Route::get('/github','WebCrawlerController@github');
+//
+//Route::get('/gitee','WebCrawlerController@gitee');
+//
+//Route::get('/snoopy','WebCrawlerController@snooy');
+//
+//Route::get('/amazon','WebCrawlerController@amazon');
+//
+//Route::get('/cookie','WebCrawlerController@cookie');
+//
+//Route::get('/checkcurl','WebCrawlerController@checkcurl');
+//
+//Route::get('/zhihu','WebCrawlerController@zhihu');
+//
+//Route::get('/test_spider','WebCrawlerController@test_spider');
 
 //TODO 开始爬虫
 Route::group(['prefix'=>'spider'],function (){
