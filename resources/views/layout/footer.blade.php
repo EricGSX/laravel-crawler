60340009443db5cea65aae58901184c31e901465
Eric.Guo <eric.guo@oceania.com>
2018-07-03 10:25:05 +0800

    scroll-to-top

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,4 +4,4 @@
     {{--<span class='btn btn-info'>TOP</span>--}}
     <p>地瓜会武术谁都挡不住  Follow me :  <a href="https://gitee.com/EricGuosx/spider"><img src="{{asset('image/gitee.gif')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:21px;height: 21px;'/></a><a href="https://github.com/EricGSX/spider"><img src="{{asset('image/github.jpg')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:34px;height: 25px;'/></a></p>
 </footer>
-<div style="position: fixed;right:1%;bottom:5px;"><a href='#' class='btn btn-primary' >TOP</a></div>
+<div style="position: fixed;right:1%;bottom:1%;"><a href='#'><img src="{{asset('image/scroll-to-top.jpg')}}" style="width:33px;height:33px;"></a></div>
