840c67d993e56a48920044335d4825495de3b131
GuoShiXin <2570890941@qq.com>
2018-07-03 22:34:33 +0900

    migrate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,16 +8,18 @@ class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
+     * 在所有的启动之后执行
      *
      * @return void
      */
     public function boot()
     {
-        //
+//        Schema::defaultStringLength(191);
     }
 
     /**
      * Register any application services.
+     * 在所有的启动之前执行
      *
      * @return void
      */
