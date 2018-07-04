b6b910350c8d3cc57588895aeb2d2b9478d2ec48
Eric.Guo <eric.guo@oceania.com>
2018-07-04 16:59:40 +0800

    修改scroll图片

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,7 +4,7 @@
     {{--<span class='btn btn-info'>TOP</span>--}}
     <p>地瓜会武术谁都挡不住  Follow me :  <a href="https://gitee.com/EricGuosx/spider"><img src="{{asset('image/gitee.gif')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:30px;height: 30px;'/></a><a href="https://github.com/EricGSX/spider"><img src="{{asset('image/github.jpg')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:34px;height: 25px;'/></a></p>
 </footer>
-<div id='scroll-to-top' style="position: fixed;right:1%;bottom:1%;"><a href='#'><img src="{{asset('image/scroll-to-top.jpg')}}" style="width:33px;height:33px;"></a></div>
+<div id='scroll-to-top' style="position: fixed;right:1%;bottom:1%;"><a href='#'><img src="{{asset('image/scroll-to-top2.jpg')}}" style="width:50%;height:50%;"></a></div>
 <script>
     $('#scroll-to-top').hide()
     $(window).scroll(function(){
