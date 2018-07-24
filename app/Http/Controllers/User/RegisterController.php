03d6cb9af5f6535e8ba778a8d94095117db9c4be
GuoShiXin <2570890941@qq.com>
2018-07-24 21:11:41 +0900

    register

--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\User;
 class RegisterController extends Controller
 {
+    //注册页面
     public function index()
     {
         return view('user.register');
     }
 
+    //注册
     public function register()
     {
-        return null;
+        $this->validate(request(),[
+            'name' => 'required|min:3|unique:users,name',
+            'email' => 'required|unique:users,email|email',
+            'password' => 'required|min:5|max:20|confirmed',
+        ]);
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password'));
+        $user = User::create(compact('name','email','password'));
+        return redirect('/user/login');
     }
 }
