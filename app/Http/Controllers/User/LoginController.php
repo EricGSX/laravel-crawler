f27d83cb46aa5c153333687d851b7c05e247460a
GuoShiXin <2570890941@qq.com>
2018-07-26 21:39:21 +0900

    登陆

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -14,11 +14,22 @@ class LoginController extends Controller
 
     public function login()
     {
-
+        $this->validate(request(),[
+            'email' => 'required|email',
+            'password' => 'required|min:5|max:20',
+            'is_remember' => 'integer'
+        ]);
+        $user = request(['email','password']);
+        $is_remember = boolval(request('is_remember'));
+        if(\Auth::attempt($user,$is_remember)){
+            return redirect('/posts');
+        }
+        return \Redirect::back()->withErrors('邮箱密码不匹配');
     }
 
     public function logout()
     {
-
+        \Auth::logout();
+        return redirect('/user/login');
     }
 }
