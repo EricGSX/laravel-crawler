2dba28e0ab10e5276c1092652aff89590bf44804
GuoShiXin <2570890941@qq.com>
2018-07-31 20:24:18 +0900

    备案号

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,6 +3,7 @@
     {{--<p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a><a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://github.com/EricGSX/spider">@Eric.Guo</a>.</p>--}}
     {{--<span class='btn btn-info'>TOP</span>--}}
     <p>地瓜会武术谁都挡不住  Follow me :  <a href="https://gitee.com/EricGuosx/spider"><img src="{{asset('image/gitee.gif')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:21px;height: 21px;'/></a><a href="https://github.com/EricGSX/spider"><img src="{{asset('image/github.jpg')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:34px;height: 25px;'/></a></p>
+    <p><a href="http://www.miitbeian.gov.cn"> 豫ICP备16038585号</a></p>
 </footer>
 <div id='scroll-to-top' style="position: fixed;right:1%;bottom:1%;"><a href='#'><img src="{{asset('image/scroll-to-top2.jpg')}}" style="width:50%;height:50%;"></a></div>
 <script>
