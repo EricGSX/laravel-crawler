d1485c0bdebcaae5a262ee95cdc327ad6e3b2f50
GuoShiXin <3234655977@qq.com>
2018-08-21 11:17:14 +0800

    设计了coding的选择样式

--- resources/views/home/create.blade.php
+++ resources/views/home/create.blade.php
@@ -6,11 +6,20 @@
                 {{csrf_field()}}
                 <div class="form-group">
                     <label>标题</label>
-                    <input name="title" type="text" class="form-control" placeholder="这里是标题">
+                    <input name="title" type="text" class="form-control" placeholder="标题。。。">
+                </div>
+                <div class="form-group">
+                    <label>摘要</label>
+                    <textarea id="description"  name="description" class="form-control" placeholder="摘要。。。"></textarea>
+                </div>
+                <div class="form-group">
+                    <div><label>编码方式</label></div>
+                    <button class='btn btn-primary eric-coding-type' type='button' name='ueditor'>富文本</button> <button class='btn btn-default eric-coding-type' type='button' name='markdown'>MarkDown</button>
+                    <input name="encoding_type" type="hidden" id='encoding_type'>
                 </div>
                 <div class="form-group">
                     <label>内容</label>
-                    <textarea id="content"  style="height:400px;max-height:500px;" name="content" class="form-control" placeholder="这里是内容"></textarea>
+                    <textarea id="content"  style="height:400px;max-height:500px;" name="content" class="form-control" placeholder="内容。。。"></textarea>
                 </div>
 @include('layout.error')
                 <button type="submit" class="btn btn-default">提交</button>
@@ -19,6 +28,14 @@
 
         </div><!-- /.blog-main -->
         <script type="text/javascript" src="{{asset('js/eric.js')}}"></script>
+    <script>
+        $('.eric-coding-type').on('click',function(){
+            $('.eric-coding-type').removeClass('btn-primary');
+            $('.eric-coding-type').addClass('btn-default');
+            $(this).addClass('btn-primary');
+            $('#encoding_type').val($(this)[0].textContent)
+        });
+    </script>
 @endsection
 
 
