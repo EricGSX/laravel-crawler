ab8daae6c8a4da54b73eefe02a6f0c7a46ebeaf9
暴走的机智郭 <3234655977@qq.com>
2018-08-23 22:52:06 +0900

    首页

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,6 +8,15 @@
         </div>
         <div>
             {{--内容面板--}}
+            <div class="list-group">
+                <a href="#" class="list-group-item active">
+                    Cras justo odio
+                </a>
+                <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item">Morbi leo risus</a>
+                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                <a href="#" class="list-group-item">Vestibulum at eros</a>
+            </div>
             <div class="panel panel-default">
               <div class="panel-body">
                             @foreach($posts as $post)
