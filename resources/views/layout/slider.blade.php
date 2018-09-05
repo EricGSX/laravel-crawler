f9f7a0c4f687c8fcf33d08ad03ff147029e2362b
GuoShiXin <3234655977@qq.com>
2018-09-05 16:02:48 +0800

    做了微调首页的轮播图

--- resources/views/layout/slider.blade.php
+++ resources/views/layout/slider.blade.php
@@ -7,15 +7,15 @@
     </ol>
     <!-- 轮播（Carousel）项目 -->
     <div class="carousel-inner">
-        <div class="item active" style="background:url(http://three.com/image/banner1.jpg); background-size:cover;height: 200px;">
+        <div class="item" style="background:url(http://three.com/image/banner1.jpg); background-size:cover;height: 200px;">
             {{--<img src="">--}}
-            <div class="carousel-caption">Stay Hungry. Stay Foolish.</div>
+            <div class="carousel-caption">Make a little progress every day.</div>
         </div>
         <div class="item" style="background:url(http://three.com/image/banner2.jpg); background-size:cover;height: 200px;">
             <div class="carousel-caption">Stay Hungry. Stay Foolish.</div>
         </div>
-        <div class="item" style="background:url(http://three.com/image/banner3.jpg); background-size:cover;height: 200px;">
-            <div class="carousel-caption">Stay Hungry. Stay Foolish.</div>
+        <div class="item active" style="background:url(http://three.com/image/banner3.jpg); background-size:cover;height: 200px;">
+            <div class="carousel-caption">Nobody asked you must is remarkable, but yourself. No one can reach excellent step, just a little bit of progress every day.</div>
         </div>
     </div>
     <!-- 轮播（Carousel）导航 -->
