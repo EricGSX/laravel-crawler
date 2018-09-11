46ff32ebab4e89fe8f9778d648374254fdc9a0c8
暴走的机智郭 <3234655977@qq.com>
2018-09-11 20:56:42 +0800

    添加了首页

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,5 @@ Route::group(['prefix'=>'admin'],function (){
     Route::get('/login','Admin\LoginController@index');
     Route::Post('/login','Admin\LoginController@login');
     Route::get('/logout','Admin\LoginController@logout');
+    Route::get('/home','Admin\HomeController@index');
 });
