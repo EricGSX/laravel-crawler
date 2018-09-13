e0359b3f3d7e4a52c033c799d2b269c3a581a4f1
GuoShiXin <3234655977@qq.com>
2018-09-13 11:25:07 +0800

    Schema添加索引

--- database/migrations/2018_07_03_132422_create_posts_table.php
+++ database/migrations/2018_07_03_132422_create_posts_table.php
@@ -16,12 +16,14 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title',100)->default('');
-            $table->text('description')->comment('摘要');
+            $table->string('description',200)->comment('摘要');
             $table->text('content');
             $table->tinyInteger('encoding_type')->default(1)->comment('1:富文本，2:markdown');
             $table->integer('view_count')->default(0)->comment('浏览数');
             $table->integer('user_id')->default(0);
             $table->timestamps();
+            $table->index('title');
+            $table->index('description');
         });
     }
 
