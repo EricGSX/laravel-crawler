c5641fa3679d5cd9804737b7b11606f7d7b1b0de
GuoShiXin <3234655977@qq.com>
2018-09-13 09:31:07 +0800

    在后台使用中间件middleware报错提示Route[login] not defined的时候可以找个路由给他命名为login，就会去跳转到这个地方

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,7 @@
 //TODO 后台
 Route::group(['prefix'=>'admin'],function (){
     Route::get('/login','Admin\LoginController@index');
-    Route::Post('/login','Admin\LoginController@login');
+    Route::Post('/login','Admin\LoginController@login')->name('login');
     Route::get('/logout','Admin\LoginController@logout');
     Route::group(['middleware' => 'auth:admin'],function (){
         //首页
