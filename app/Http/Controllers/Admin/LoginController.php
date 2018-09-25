68cabfeeccaf7b66f6a1695c3c0a9a9b64abe96a
GuoShiXin <3234655977@qq.com>
2018-09-25 17:50:53 +0800

    修复了之前因为失误造成的错误

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -43,7 +43,7 @@ class LoginController extends Controller
      */
     public function logout()
     {
-        \Auth::guard('admin')-id>logout();
+        \Auth::guard('admin')->logout();
         return redirect('/admin/login');
     }
 }
