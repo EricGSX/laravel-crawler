63e12ef7c9355e76d1709cb463ed86405e83e507
GuoShiXin <3234655977@qq.com>
2018-09-26 10:10:10 +0800

    增加了bail验证规则， 让在第一个验证失败后不执行之后的验证操作

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -25,8 +25,8 @@ class LoginController extends Controller
     public function login()
     {
         $this->validate(request(),[
-            'name' => 'required|min:2',
-            'password' => 'required|min:5|max:20',
+            'name' => 'bail|required|min:2',
+            'password' => 'bail|required|min:5|max:20',
         ]);
         $user = request(['name','password']);
         if(\Auth::guard('admin')->attempt($user)){
