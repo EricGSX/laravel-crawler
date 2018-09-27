575a97212f21dc032cd46f3f59ccb2d4336df9df
GuoShiXin <3234655977@qq.com>
2018-09-27 18:47:14 +0800

    将adminlte升级到2.4

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -2,51 +2,31 @@
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
-    @include('admin.layout.header')
-     <!-- Left side column. contains the logo and sidebar -->
-    @include('admin.layout.sidebar')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
 
-        <!-- Main content -->
-        @yield('content')
-        <!-- /.content -->
+  @include('admin.layout.header')
+  <!-- Left side column. contains the logo and sidebar -->
+
+  @include('admin.layout.sidebar')
+  <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+      @yield('content')
+  </div>
+      <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.0
     </div>
-    <!-- /.content-wrapper -->
-    <!-- /.control-sidebar -->
-    <!-- Add the sidebar's background. This div must be placed
-         immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+  </footer>
+
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery 2.2.3 -->
-<script src="/adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.6 -->
-<script src="/adminlte/bootstrap/js/bootstrap.min.js"></script>
-<!-- Sparkline -->
-<script src="/adminlte/plugins/sparkline/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="/adminlte/plugins/knob/jquery.knob.js"></script>
-<!-- daterangepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="/adminlte/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="/adminlte/plugins/datepicker/bootstrap-datepicker.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="/adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="/adminlte/plugins/fastclick/fastclick.js"></script>
+<!-- jQuery 3 -->
+<script src="/adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/adminlte/dist/js/adminlte.min.js"></script>
 </body>
 </html>
--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,38 +1,110 @@
-<aside class="main-sidebar">
+{{--<aside class="main-sidebar">--}}
+    {{--<!-- sidebar: style can be found in sidebar.less -->--}}
+    {{--<section class="sidebar">--}}
+        {{--<!-- /.search form -->--}}
+        {{--<!-- sidebar menu: : style can be found in sidebar.less -->--}}
+        {{--<ul class="sidebar-menu">--}}
+            {{--<li class="treeview active">--}}
+                {{--<a href="#" >--}}
+                    {{--<i class="fa fa-dashboard"></i> <span>系统管理</span>--}}
+                    {{--<span class="pull-right-container">--}}
+                        {{--<i class="e-top-down glyphicon glyphicon-chevron-down" show-value="1"></i>--}}
+                    {{--</span>--}}
+                {{--</a>--}}
+                {{--<ul class="treeview-menu">--}}
+                    {{--<li><a href="/admin/permissions"><i class="fa fa-circle-o"></i> 权限管理</a></li>--}}
+                    {{--<li><a href="/admin/users"><i class="fa fa-circle-o"></i> 用户管理</a></li>--}}
+                    {{--<li><a href="/admin/roles"><i class="fa fa-circle-o"></i> 角色管理</a></li>--}}
+                {{--</ul>--}}
+            {{--</li>--}}
+            {{--<li class=" treeview">--}}
+                {{--<a href="/admin/posts">--}}
+                    {{--<i class="fa fa-dashboard"></i> <span>文章管理</span>--}}
+                {{--</a>--}}
+            {{--</li>--}}
+            {{--<li class=" treeview">--}}
+                {{--<a href="/admin/topics">--}}
+                    {{--<i class="fa fa-dashboard"></i> <span>专题管理</span>--}}
+                {{--</a>--}}
+            {{--</li>--}}
+            {{--<li class=" treeview">--}}
+                {{--<a href="/admin/notices">--}}
+                    {{--<i class="fa fa-dashboard"></i> <span>通知管理</span>--}}
+                {{--</a>--}}
+            {{--</li>--}}
+        {{--</ul>--}}
+    {{--</section>--}}
+    {{--<!-- /.sidebar -->--}}
+{{--</aside>--}}
+  <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
-        <!-- /.search form -->
-        <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu">
-            <li class="treeview active">
-                <a href="#" >
-                    <i class="fa fa-dashboard"></i> <span>系统管理</span>
-                    <span class="pull-right-container">
-                        <i class="e-top-down glyphicon glyphicon-chevron-down" show-value="1"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="/admin/permissions"><i class="fa fa-circle-o"></i> 权限管理</a></li>
-                    <li><a href="/admin/users"><i class="fa fa-circle-o"></i> 用户管理</a></li>
-                    <li><a href="/admin/roles"><i class="fa fa-circle-o"></i> 角色管理</a></li>
-                </ul>
-            </li>
-            <li class=" treeview">
-                <a href="/admin/posts">
-                    <i class="fa fa-dashboard"></i> <span>文章管理</span>
-                </a>
-            </li>
-            <li class=" treeview">
-                <a href="/admin/topics">
-                    <i class="fa fa-dashboard"></i> <span>专题管理</span>
-                </a>
-            </li>
-            <li class=" treeview">
-                <a href="/admin/notices">
-                    <i class="fa fa-dashboard"></i> <span>通知管理</span>
-                </a>
-            </li>
-        </ul>
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="/adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Alexander Pierce</p>
+          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- search form -->
+
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="treeview active">
+          <a href="#">
+            <i class="fa fa-dashboard"></i>
+            <span>Dashboard</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+            <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Charts</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+            <li><a href="morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
+            <li><a href="flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
+            <li><a href="inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Charts</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
+            <li><a href="morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
+            <li><a href="flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
+            <li><a href="inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+          </ul>
+        </li>
+
+        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        <li class="header">LABELS</li>
+        <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+      </ul>
     </section>
     <!-- /.sidebar -->
-</aside>
+  </aside>
