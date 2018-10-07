595108756e113b59c45c37b619d2a25e47aad30e
暴走的机智郭 <3234655977@qq.com>
2018-10-07 20:52:12 +0800

    对用户的信息进行了处理，让本人无法删除本人，添加了角色数据库

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -24,7 +24,11 @@
                                     <td>{{$user->name}}</td>
                                     <td>
                                         <a type="button" class="btn" href="/admin/users/{{$user->id}}/role" >角色管理</a>
+                                        @if($user->name != \Auth::guard('admin')->user()->name)
                                         <a type="button" class="btn glyphicon glyphicon-remove" href="/admin/users/{{$user->id}}/delete" ></a>
+                                        @else
+                                            <a type="button" class="btn glyphicon glyphicon-user"></a>
+                                        @endif
                                         <a type="button" class="btn glyphicon glyphicon-pencil" href="/admin/users/{{$user->id}}/update" ></a>
                                     </td>
                                 </tr>
