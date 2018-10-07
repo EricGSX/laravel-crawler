c237bd15ea04524a13152bdc2fd4f5444aac786a
暴走的机智郭 <3234655977@qq.com>
2018-10-07 21:02:55 +0800

    增加了角色模型

---
+++ app/AdminPermission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\Model;
+
+class AdminPermission extends Model
+{
+    protected $table = 'admin_permissions';
+}
---
+++ app/AdminRole.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use App\Model;
+
+class AdminRole extends Model
+{
+    protected $table = 'admin_roles';
+}
