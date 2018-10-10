5e490a2397c351f11ae0cc92e0b3ceccb246218b
暴走的机智郭 <3234655977@qq.com>
2018-10-10 22:32:26 +0800

    模型开始写了

--- app/AdminRole.php
+++ app/AdminRole.php
@@ -7,4 +7,48 @@ use App\Model;
 class AdminRole extends Model
 {
     protected $table = 'admin_roles';
+
+    protected $guarded = [];
+
+    /**
+     * 当前角色的所有权限
+     * 
+     * @return [type] [description]
+     */
+    public function permissions()
+    {
+    	$this->belongsToMany(\App\AdminPermission::class,'admin_permission_role','role_id','permission_id')->withPivat(['permission_id','role_id']);
+    }
+
+    /**
+     * 角色赋予权限
+     * 
+     * @param  [type] $permission [description]
+     * @return [type]             [description]
+     */
+    public function grantPermission($permission)
+    {
+    	return $this->permissions()->save($permission);
+    }
+
+    /**
+     * 取消角色赋予的权限
+     * @param  [type] $permission [description]
+     * @return [type]             [description]
+     */
+    public function deletePermission($permission)
+    {
+    	return $this->permissions()->detach($permission);
+    }
+
+    /**
+     * 判断角色是否有权限
+     * 
+     * @param  [type]  $permission [description]
+     * @return boolean             [description]
+     */
+    public function hasPermission($permission)
+    {
+    	return $this->permissions()->contains($permissions);
+    }
 }
--- app/AdminUser.php
+++ app/AdminUser.php
@@ -12,4 +12,46 @@ class AdminUser extends Authenticatable
 
     //TODO 设置不可注入的字段为空
     protected $guarded = [];
+
+    /**
+     * TODO 用户有哪些角色
+     * @return [type] [description]
+     */
+    public function roles()
+    {
+    	return $this->belongsToMany(\App\AdminRole::class,'admin_role_user','user_id','role_id')->withPivot(['user_id','role_id']);
+    }
+
+    /**
+     * 判断是否有某个、某些角色
+     */
+    public function isInRoles($roles)
+    {
+    	return !!$roles->intersect($this->roles)->count();
+    }
+
+    /**
+     * 给用户分配角色
+     */
+    public function assignRole($role)
+    {
+    	return $this->roles()->save($role);
+    }
+
+    /**
+     * 取消用户分配的角色
+     * 
+     * @param  [type] $tole [description]
+     * @return [type]       [description]
+     */
+    public function deleteRole($tole)
+    {
+    	return $this->roles()->detach($role);
+    }
+
+    public function hasPermission($permission)
+    {
+    	//TODO 
+    }
+
 }
