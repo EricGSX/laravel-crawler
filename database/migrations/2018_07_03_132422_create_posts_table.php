b033ae201348744a083783b3d7a730f138f87cfe
GuoShiXin <3234655977@qq.com>
2018-11-02 18:02:16 +0800

    添加了一个字段说明

--- database/migrations/2018_07_03_132422_create_posts_table.php
+++ database/migrations/2018_07_03_132422_create_posts_table.php
@@ -21,7 +21,7 @@ class CreatePostsTable extends Migration
             $table->tinyInteger('encoding_type')->default(1)->comment('1:富文本，2:markdown');
             $table->integer('view_count')->default(0)->comment('浏览数');
             $table->integer('user_id')->default(0);
-            $table->tinyInteger('mark_status')->default(0)->comment('标记的状态，0默认-1软删除1标记');
+            $table->tinyInteger('mark_status')->default(0)->comment('标记的状态，0默认-1软删除1标记2动态标签云');
             $table->timestamps();
             $table->softDeletes();
             $table->index('title');
