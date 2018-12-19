b431b0863b3c0bf467ba5193e75f14b206a81bad
GuoShiXin <3234655977@qq.com>
2018-12-19 17:50:05 +0800

    添加了sidebar的发送邮件页面

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -78,6 +78,48 @@
             </li>
         </ul>
     </aside>
+        <aside id="widget-fature" class="widget panel panel-default">
+        <div class="panel-heading" style='text-align: center;'>
+            建议&反馈
+        </div>
+        <div class="row">
+            <style>
+                .eric-form{
+                    border-top:0px solid #fff!important;border-left:0px solid #fff!important;border-right:0px solid #fff!important;border-radius:0px!important;border-bottom:1px solid #dcdcdc!important;box-shadow: none!important;
+                }
+            </style>
+            <form action="/posts" method="POST">
+                {{csrf_field()}}
+                <div class="col-sm-12">
+                    <div class="col-sm-1">
+                    </div>
+                    <div class="col-sm-10">
+                        <input value="" class="form-control eric-form" name='emial_user' placeholder="UserName">
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-1">
+                    </div>
+                    <div class="col-sm-10">
+                        <input value="" class="form-control eric-form" name='email_address' placeholder="Email">
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="col-sm-1">
+                    </div>
+                    <div class="col-sm-10">
+                        <textarea class="form-control eric-form" name="email_commits"  placeholder="Commits"></textarea>
+                    </div>
+                </div>
+                <div class="col-sm-12" style="margin:8px;">
+                    <div class="col-sm-8">
+                        @include('layout.error')
+                        <button type="submit" class="btn btn-default col-sm-6 col-xs-12">Go<label class="glyphicon glyphicon-send"></label></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </aside>
 
 </div>
 <style>
