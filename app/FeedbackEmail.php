1fd0f4dae2df1f659ab57197bad6a07f4ce716d0
GuoShiXin <3234655977@qq.com>
2018-12-20 10:55:05 +0800

    修改了模型，添加了一个方法

--- app/FeedbackEmail.php
+++ app/FeedbackEmail.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use App\Model;
+use Illuminate\Support\Facades\Mail;
 
 class FeedbackEmail extends Model
 {
-    //
+    /**
+     * TODO 自动回复信息
+     *
+     * @param array $data
+     * @return bool
+     */
+    public function autoReply($data=[])
+    {
+        try{
+            $message = $data['msg'];
+            $to = $data['to'];
+            $subject = '自动回复';
+            $result = Mail::send(
+                'emails.test',
+                ['content' => $message],
+                function ($message) use($to, $subject) {
+                    $message->to($to)->subject($subject);
+                }
+            );
+            if($result){
+                return true;
+            }else{
+                return false;
+            }
+        }catch (\Throwable $e){
+            return false;
+        }
+    }
 }
