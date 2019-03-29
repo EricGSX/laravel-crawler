5a3b5db43ef0fb163b6ed73da821c87a51b5c69c
GuoShiXin <3234655977@qq.com>
2019-03-29 15:19:14 +0800

    新增了gitbook

--- resources/views/layout/chat.blade.php
+++ resources/views/layout/chat.blade.php
@@ -1,21 +1,26 @@
 <p><a href="javascript:void(0)" style="text-decoration: none;">click and chat with me&nbsp;&nbsp;&nbsp;&nbsp;<i class="glyphicon glyphicon-hand-down"></i></a></p>
 <div class="row">
-	<div class="col-sm-3">
+	<div class="col-sm-1"></div>
+	<div class="col-sm-2">
 		<div tabindex="0"  role="button" data-toggle="popover" data-trigger="hover" data-html="true" data-placement="bottom" data-content='   
         <img style="180px;height:180px;" src="/image/wechat_small.jpg">
 '>
 		    <div><img src="{{asset('/image/wx.png')}}" style="width:35px;height:35px;"></div>
 		</div>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-2">
 		<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=2570890941&site=qq&menu=yes" title="QQ在线聊天"><img src="{{asset('/image/qq.png')}}" style="width:35px;height:35px;"></a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-2">
         <a target="_blank" href="https://weibo.com/2980765271/profile?rightmod=1&wvr=6&mod=personinfo" title="微博"><img src="{{asset('/image/weibo.png')}}" style="width:35px;height:35px;"></a>
 	</div>
-	<div class="col-sm-3">
+	<div class="col-sm-2">
         <a target="_blank" href="https://segmentfault.com/u/huiwushudedigua" title="SegmentFault思否"><img src="{{asset('/image/sf.jpg')}}" style="width:35px;height:35px;" class="img-circle"></a>
 	</div>
+	<div class="col-sm-2">
+        <a target="_blank" href="https://book.guosx.com/" title="GitBook"><img src="{{asset('/image/gitbook.png')}}" style="width:35px;height:35px;"></a>
+	</div>
+	<div class="col-sm-1"></div>
 </div>
 <script>
 $(function () {
