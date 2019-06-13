01c08720600d684cff7bba4a5c247f5f329955ce
GuoShiXin <3234655977@qq.com>
2019-06-13 10:37:11 +0800

    修改工信部地址

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,7 +3,7 @@
     {{--<p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a><a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://github.com/EricGSX/spider">@Eric.Guo</a>.</p>--}}
     {{--<span class='btn btn-info'>TOP</span>--}}
     <span> Follow me :  <a href="https://gitee.com/EricGuosx/spider"><img src="{{asset('image/gitee.gif')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:21px;height: 21px;'/></a><a href="https://github.com/EricGSX/spider"><img src="{{asset('image/github.jpg')}}" alt="开源是一种精神-GuoShiXin" title="开源是一种精神-GuoShiXin" style='width:34px;height: 25px;'/></a></span>
-    <span style="padding-left: 50px;">&copy;2018 guosx.com All rights reserved. &nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.miitbeian.gov.cn"> 豫ICP备16038585号-1</a></span>
+    <span style="padding-left: 50px;">&copy;2018 guosx.com All rights reserved. &nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.beian.miit.gov.cn"> 豫ICP备16038585号-1</a></span>
 </footer>
 <footer class="blog-footer">
     <span>Friendly Link :
