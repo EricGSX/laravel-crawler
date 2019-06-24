ac3500f919256afdba37f7380c11c6e602b12e76
暴走的机智郭 <3234655977@qq.com>
2019-06-24 22:26:47 +0800

    修改数据库结构，生成数据库迁移

---
+++ database/migrations/2019_06_24_221613_change_users_third_login_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUsersThirdLoginTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('user_img',200)->nullable()->after('remember_token');
+            $table->integer('platform_uid')->after('remember_token');
+            $table->string('platform_type',100)->after('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_img');
+            $table->dropColumn('platform_uid');
+            $table->dropColumn('platform_type');
+        });
+    }
+}
